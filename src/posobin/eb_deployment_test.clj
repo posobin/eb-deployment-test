ffcbd6481b8826fc977bcc02bc84c709c03323a4
Gleb Posobin <posobin@gmail.com>
2021-01-25 01:43:41 -0500

    Add python

--- src/posobin/eb_deployment_test.clj
+++ src/posobin/eb_deployment_test.clj
@@ -1,6 +1,7 @@
 (ns posobin.eb-deployment-test
   (:require
     [ring.adapter.jetty :as jetty]
+    [libpython-clj.python :refer [py.-] :as py]
     [cprop.core :refer [load-config]])
   (:gen-class))
 
@@ -19,5 +20,11 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& _args]
+  (when (:prod config)
+    (py/initialize! :python-executable "virtualenv/python3"))
+  ((requiring-resolve 'libpython-clj.require/require-python) 'requests)
+  (println (-> ((requiring-resolve 'requests/get) "https://ya.ru"
+                {"Accept-Encoding" "identity"})
+             (py.- :headers)))
   (println "Serving on port" (config :port 3000))
   (jetty/run-jetty handler {:port (config :port 3000)}))
